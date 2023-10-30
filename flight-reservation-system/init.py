adc0cd2bcf1bf2acaa54a9615b1650f8c3aa9afc
superkosat <36095695+superkosat@users.noreply.github.com>
2023-10-30 18:22:39 +0800

    added final project directory
    added files, source code, etc. under new directory for airline reservation system

---
+++ flight-reservation-system/init.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, session, request, url_for, redirect
+import pymysql.cursors
+
+app = Flask(__name__)
+
+#Establish connection to MySQL server
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='',
+                       db='airline_test_3',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+#Define routes
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        username = session['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+#route for login authentication
+@app.route('/loginAuth', methods=['GET', 'POST'])
+def loginAuth():
+    username = request.form['username']
+    password = request.form['password']
+
+    cursor = conn.cursor()
+    query = 'SELECT * FROM customer WHERE email = %s AND password = %s'
+    cursor.execute(query, (username, password))
+
+    data = cursor.fetchone()
+
+    cursor.close()
+    error = None
+    if (data):
+        session['username'] = username
+        return redirect(url_for('index'))
+    else:
+        error = 'Invalid username or password'
+        return render_template('login.html', error=error)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+#Define route to display all upcoming flights in the database
+@app.route('/display_upcoming')
+def display_upcoming():
+    cursor = conn.cursor()
+    query = 'SELECT airline_name, flight_num, dep_airport, arr_airport FROM flight WHERE status = "upcoming"'
+    cursor.execute(query)
+
+    data = cursor.fetchall()
+
+    cursor.close()
+    error = None
+    if (data):
+        return render_template('display_upcoming.html', data=data)
+    else:
+        error = 'No upcoming flights found'
+        return render_template('index.html', error=error)
+
+
+app.secret_key = 'H2O intolerant'
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 5000, debug=True)
