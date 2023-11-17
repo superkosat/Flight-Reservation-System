36b91cd7532f0f7ef6169306bc649f23f251e90b
superkosat <36095695+superkosat@users.noreply.github.com>
2023-11-18 04:05:51 +0800

    added features
    added search functionality from home page, added permissions and full login functionality

--- flight-reservation-system/init.py
+++ flight-reservation-system/init.py
@@ -11,56 +11,33 @@ conn = pymysql.connect(host='localhost',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
+### context processors and macro functions ###
+
+def is_logged_in():
+    return 'username' in session
+
+@app.context_processor
+def inject_user():
+    return dict(is_logged_in=is_logged_in)
+
+
+
 ### Define routes ###
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        username = session['username']
-        return redirect(url_for('home'))
-    return render_template('login.html')
+    return redirect(url_for('home'))
 
 @app.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/search')
+def search():
+    return
 
-@app.route('/logout')
-def logout():
-    session.clear
-    return redirect(url_for('login'))
-
-#route for login authentication
-@app.route('/loginAuth', methods=['GET', 'POST'])
-def loginAuth():
-    username = request.form['username']
-    password = request.form['password']
-
-    cursor = conn.cursor()
-    query = 'SELECT * FROM customer WHERE email = %s AND password = %s'
-    cursor.execute(query, (username, password))
-
-    data = cursor.fetchone()
-
-    cursor.close()
-    error = None
-    if (data):
-        session['username'] = username
-        return redirect(url_for('display_upcoming'))
-    else:
-        error = 'Invalid username or password'
-        return render_template('login.html', error=error)
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-#Define route to display all upcoming flights in the database
-@app.route('/display_upcoming')
-def display_upcoming():
+@app.route('/searchFlights', methods=['GET', 'POST'])
+def searchFlights():
     cursor = conn.cursor()
     query = 'SELECT airline_name, flight_num, dep_airport, arr_airport FROM flight WHERE status = "upcoming"'
     cursor.execute(query)
@@ -75,10 +52,85 @@ def display_upcoming():
         error = 'No upcoming flights found'
         return render_template('index.html', error=error)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+#route for login authentication
+@app.route('/loginAuth', methods=['GET', 'POST'])
+def loginAuth():
+    username = request.form['username']
+    password = request.form['password']
+
+    #SELECT * FROM customer WHERE email = %s AND password = %s
+    cursor = conn.cursor()
+    query = '''
+        SELECT 'customer' AS user_type, email, password FROM customer WHERE email = %s AND password = %s
+        UNION
+        SELECT 'airline_staff' AS user_type, username AS email, password FROM airline_staff WHERE username = %s AND password = %s
+        UNION
+        SELECT 'booking_agent' AS user_type, email, password FROM booking_agent WHERE email = %s AND password = %s
+    '''
+    cursor.execute(query, (username, password, username, password, username, password))
+
+    data = cursor.fetchone()
+
+    cursor.close()
+    error = None
+    if (data):
+        session['username'] = username
+        session['permission'] = data['user_type']
+        return redirect(url_for('home'))
+    else:
+        error = 'Invalid username or password'
+        return render_template('login.html', error=error)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+#Define route to display all upcoming flights in the database
+@app.route('/displayUpcoming')
+def displayUpcoming():
+    cursor = conn.cursor()
+    query = 'SELECT airline_name, flight_num, dep_airport, arr_airport FROM flight WHERE status = "upcoming"'
+    cursor.execute(query)
+
+    data = cursor.fetchall()
+
+    cursor.close()
+    error = None
+    if (data):
+        return render_template('display_upcoming.html', data=data)
+    else:
+        error = 'No upcoming flights found'
+        return render_template('index.html', error=error)
+
+#define route to display purchased flights
+#MUST BE LOGGED IN WITH CUSTOMER PERMISSION
+@app.route('/my_flights')
+def my_flights():
+    return render_template('customer_flights.html')
+
+
+
+
 ### error handling ###
 @app.errorhandler(404)  
 def not_found(e):
     return render_template('404.html')
+
+@app.errorhandler(400)  
+def not_found(e):
+    return render_template('400.html')
+
+
+
 
 #temporary routes for development REMOVE 
 @app.route('/base')
@@ -88,6 +140,9 @@ def base():
 @app.route('/test')
 def test():
     return render_template('test.html')
+
+
+
 
 #initialization
 app.secret_key = 'H2O intolerant'
